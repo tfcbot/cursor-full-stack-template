4dccd4cf61368bfe5764d432345915c43f8b0723
tfcbot <admin@blurware.com>
2025-04-12 10:07:35 -0400

    update

--- infra/secrets.ts
+++ infra/secrets.ts
@@ -5,20 +5,18 @@ export const stripeWebhookSecret = new sst.Secret('StripeWebhookSecret')
 export const stripePublishableKey = new sst.Secret('StripePublishableKey')
 export const unkeyApiId = new sst.Secret('UnkeyApiId')
 export const unkeyRootKey = new sst.Secret('UnkeyRootKey')
-
-export const secrets = [openaiApiKey, stripeSecretKey, stripeWebhookSecret, stripePublishableKey, unkeyApiId, unkeyRootKey]
-const openaiApiKey = new sst.Secret("OpenAiApiKey")
-const clerkClientPublishableKey = new sst.Secret("ClerkClientPublishableKey")
-const clerkClientSecretKey = new sst.Secret("ClerkClientSecretKey")
-const clerkWebhookSecret = new sst.Secret("ClerkWebhookSecret")
+export const clerkClientPublishableKey = new sst.Secret("ClerkClientPublishableKey")
+export const clerkClientSecretKey = new sst.Secret("ClerkClientSecretKey")
+export const clerkWebhookSecret = new sst.Secret("ClerkWebhookSecret")
 
 export const secrets = [
     openaiApiKey,
     clerkClientPublishableKey,
     clerkClientSecretKey,
-    clerkWebhookSecret
+    clerkWebhookSecret,
+    stripeSecretKey,
+    stripeWebhookSecret,
+    stripePublishableKey,
+    unkeyApiId,
+    unkeyRootKey
 ]
-
-export const ClerkClientPublishableKey = clerkClientPublishableKey
-export const ClerkClientSecretKey = clerkClientSecretKey
-export const ClerkWebhookSecret = clerkWebhookSecret
