0c005bc1b5a283fdc974983829bd373e6b40c058
tfcbot <admin@blurware.com>
2025-04-13 08:07:08 -0400

    fix build errors

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').Config} */
 const nextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_CLERK_CLIENT_PUBLISHABLE_KEY,
+  },
 };
 
 module.exports = nextConfig;
